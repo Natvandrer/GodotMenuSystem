273403ae813168727045a9da15918bccba0d2b5d
Henrik Hampenberg <hvh@inxhaine.com>
2026-06-04 06:58:33 +0200

    Texture buttons added

--- godot-menu-system/Scripts/menu.gd
+++ godot-menu-system/Scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")    #The file to load
+	get_tree().change_scene_to_file("res://Scenes/PaintedButtons.tscn")    #The file to load
 
 func _on_option_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")    #The file to load
